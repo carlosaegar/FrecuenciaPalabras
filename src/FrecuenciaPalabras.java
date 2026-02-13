4deae5a2dab4da4653d58d8b1d8ae78b20312497
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-13 10:54:25 +0100

    Update FrecuenciaPalabras.java

--- src/FrecuenciaPalabras.java
+++ src/FrecuenciaPalabras.java
@@ -6,17 +6,14 @@ public class FrecuenciaPalabras {
         String texto = "Hola este es un texto de prueba para ver las palabras que se repiten en " +
                 "el texto y las veces que estan en el texto";
 
-        String[] palabras = texto.toLowerCase().split("\\s+");
+        String[] palabras = texto.toLowerCase().split(" ");
 
-        // 2. Crear el mapa de frecuencias
         HashMap<String, Integer> frecuencias = new HashMap<>();
 
         for (String p : palabras) {
-            // Si existe suma 1, si no, empieza en 1
             frecuencias.put(p, frecuencias.getOrDefault(p, 0) + 1);
         }
 
-        // 3. Mostrar resultados
         System.out.println("Frecuencia de palabras:");
         for (Map.Entry<String, Integer> entrada : frecuencias.entrySet()) {
             System.out.println(entrada.getKey() + ": " + entrada.getValue());
